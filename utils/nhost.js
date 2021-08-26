f6d62a1a8db57475876fb0c302d9c105f60c6e7f
kings431 <marc_walk@hotmail.com>
2021-08-26 12:12:40 -0300

    nhost added

--- utils/nhost.js
+++ utils/nhost.js
@@ -1,4 +1,4 @@
-import nhost from "nhost-js-sdk";
+/*import nhost from "nhost-js-sdk";
 
 const config = {
   base_url: process.env.NEXT_PUBLIC_NHOST_BACKEND,
@@ -10,3 +10,21 @@ const auth = nhost.auth();
 const storage = nhost.storage();
 
 export { auth, storage };
+
+console.log(connfig.base_url);*/
+
+
+
+//Please specify a baseURL. More information at https://docs.nhost.io/libraries/nhost-js-sdk#setup. error?
+
+import { createClient } from "nhost-js-sdk";
+
+const config = {
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_ENDPOINT,
+};
+
+const { auth, storage } = createClient(config);
+
+export { auth, storage };
+
+console.log(process.env.NEXT_PUBLIC_BACKEND_ENDPOINT);
